6a751640bf03a5e6c2a2e436953c0aac8fd66cb7
Rosano Coutinho <github.com@xeari.com>
2025-10-17 11:55:38 +0100

    Build 8f6cc05

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
 
 			// DATA
 
-			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1760698411745',
+			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1760698493508',
 			_DataPersistenceCacheName: 'OLSKServiceWorkerPersistenceCache',
 			_DataOriginPage: '/review',
 
